ffdf32d382726d4290afc6a21aa6a49cd490cbb8
Josue <78059379+jlemus1367@users.noreply.github.com>
2021-05-19 17:28:01 -0500

    Fixed filter button

--- static/js/app.js
+++ static/js/app.js
@@ -29,7 +29,7 @@ function handleClick() {
     let date = d3.select("#datetime").property("value");
     let filteredData = tableData;
 
-    // Check to see if a date was entered and filter the 
+     // Check to see if a date was entered and filter the 
     // data using the date.
     if (date) {
       // Apply `filter` to the table data to only keep the
@@ -37,14 +37,14 @@ function handleClick() {
       filteredData = filteredData.filter(row => row.datetime === date);
     };
 
-    // Rebuild the table using the filtered data
+     // Rebuild the table using the filtered data
     // @NOTE: If no date was entered, then filteredData will
     // just be the original tableData
     buildTable(filteredData);
 };
 
 // Attach an event to listen for the form button
-d3.selectAll("filter-btn").on("click", handleClick);
+d3.selectAll("#filter-btn").on("click", handleClick);
 
 // Build the table when the page loads
 buildTable(tableData);
